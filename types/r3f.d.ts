8dda0ab23f92dc4b791a54d6217189d2eb5fa99a
sufiyan-sabeel <riyazo65ckm@gmail.com>
2026-07-17 17:57:05 +0530

    fix: Use triple-slash reference for R3F JSX types

--- types/r3f.d.ts
+++ types/r3f.d.ts
@@ -1,15 +1,2 @@
-/**
- * Augment JSX IntrinsicElements with @react-three/fiber's Three.js elements.
- * This is needed because tsconfig excludes node_modules but @react-three/fiber
- * declares these elements as global JSX augmentations in its types.
- */
-import "three";
-import { type ThreeElements } from "@react-three/fiber";
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace JSX {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface IntrinsicElements extends ThreeElements {}
-  }
-}
+/// <reference types="@react-three/fiber" />
+/// <reference types="three" />
